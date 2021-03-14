a69409e1091a8ec697026f832d4ac12232b7135e
Zakaria Kebairia <4.kebairia@gmail.com>
2021-03-14 01:44:23 +0100

    chaging dmenufont to Fira Code

--- config.h
+++ config.h
@@ -8,7 +8,7 @@ static const unsigned int snap      = 32;       /* snap pixel */
 static const int showbar            = 1;        /* 0 means no bar */
 static const int topbar             = 1;        /* 0 means bottom bar */
 static const char *fonts[]          = { "Fira Sans:size=11" };
-static const char dmenufont[]       = "monospace:size=13";
+static const char dmenufont[]       = "Fira Code:size=12";
 static const char col_gray1[]       = "#1d2021";
 static const char col_gray2[]       = "#444444";
 static const char col_gray3[]       = "#bbbbbb";
