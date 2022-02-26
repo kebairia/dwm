e8dbab0957f4bf86b4a31dee87fb7aaeb793dfd0
Zakaria Kebairia <4.kebairia@gmail.com>
2022-02-26 17:50:12 +0100

    chanching colors

--- config.h
+++ config.h
@@ -7,11 +7,11 @@ static const unsigned int borderpx  = 1;        /* border pixel of windows */
 static const unsigned int snap      = 32;       /* snap pixel */
 static const int showbar            = 1;        /* 0 means no bar */
 static const int topbar             = 1;        /* 0 means bottom bar */
-static const char *fonts[]          = { "Fira Code Nerd Font:size=10" };
+static const char *fonts[]          = { "Iosevka Nerd Font:size=10" };
 static const char dmenufont[]       = "Fira Code:size=10";
 static const char col_gray1[]       = "#171717";
 static const char col_gray2[]       = "#444444";
-static const char col_gray3[]       = "#bbbbbb";
+static const char col_gray3[]       = "#aaaaaa";
 static const char col_gray4[]       = "#eeeeee";
 static const char col_cyan[]        = "#005577";
 static const char *colors[][3]      = {
